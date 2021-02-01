481e0c2f4add2b9ee960b6a3f4e9973059ae6425
JoshuaMK <joshuamkw2002@hotmail.com>
2021-02-01 03:21:17 -0600

    :construction: modify buildscript, add dependency

--- build.py
+++ build.py
@@ -1,32 +1,39 @@
 import os
+import subprocess
 import sys
-from cx_Freeze import setup, Executable
 from pyisotools import __author__, __version__
 
-include_files = []
-excludes = [ "tkinter" ]
-packages = []
+if len(sys.argv) == 1 or sys.argv[1].lower() == "p":
+    subprocess.Popen('pyinstaller --noconfirm --onefile --console --icon "pyisotools/gui/icons/pyisotools.ico" --name "pyisotools" --clean --add-data "pyisotools/gui/themes;themes/"  "pyisotools/__main__.py"')
+elif sys.argv[1].lower() == "build":
+    from cx_Freeze import setup, Executable
 
-options = {
-    "build_exe": {
-        "optimize": 4,
-        "excludes": excludes,
-        "packages": packages,
-        "include_files": include_files
+    include_files = []
+    excludes = [ "tkinter" ]
+    packages = []
+
+    options = {
+        "build_exe": {
+            "optimize": 4,
+            "excludes": excludes,
+            "packages": packages,
+            "include_files": include_files
+        }
     }
-}
 
-executable = Executable(
-    script=os.path.join("pyisotools", "__main__.py"),
-    targetName="pyisotools",
-    icon=os.path.join("pyisotools", "gui", "icons", "pyisotools.ico")
-)
+    executable = Executable(
+        script=os.path.join("pyisotools", "__main__.py"),
+        targetName="pyisotools",
+        icon=os.path.join("pyisotools", "gui", "icons", "pyisotools.ico")
+    )
 
-setup(name = "pyisotools",
-      version = __version__,
-      description = "DOL Patcher for extending the codespace of Wii/GC games",
-      executables = [executable],
-      author = __author__,
-      author_email = "joshuamkw2002@gmail.com",
-      options = options
-      )
+    setup(name = "pyisotools",
+        version = __version__,
+        description = "DOL Patcher for extending the codespace of Wii/GC games",
+        executables = [executable],
+        author = __author__,
+        author_email = "joshuamkw2002@gmail.com",
+        options = options
+        )
+else:
+    raise ValueError(f"Invalid arg {sys.argv[1]} is not `p' or `build'")
