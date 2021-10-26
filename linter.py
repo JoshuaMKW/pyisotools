f4c907fdf71452152f09a716c6a376ab7a9d3684
JoshuaMK <joshuamkw2002@gmail.com>
2021-10-26 03:12:58 -0500

    Add linter

---
+++ linter.py
@@ -0,0 +1,59 @@
+import argparse
+import os
+
+from glob import glob
+import sys
+from typing import Optional, Tuple
+from pylint.lint import Run
+
+
+class CodeQualityError(Exception):
+    ...
+
+
+def lint(path: str, threshold: float):
+    results = Run(["pyisotools"], do_exit=False)
+    score = results.linter.stats["global_note"]
+
+    if score < threshold or results.linter.msg_status != 0:
+        os.environ["PYLINT_COLOR"] = "red"
+        os.environ["PYLINT_VALUE"] = "failing"
+        raise CodeQualityError(
+            f"Code quality is too poor [{score} < {threshold}]")
+
+    if score < 3:
+        os.environ["PYLINT_COLOR"] = "red"
+    elif score < 5:
+        os.environ["PYLINT_COLOR"] = "orange"
+    elif score < 7:
+        os.environ["PYLINT_COLOR"] = "yellow"
+    else:
+        os.environ["PYLINT_COLOR"] = "green"
+        os.environ["PYLINT_VALUE"] = score
+
+
+def main(args: Optional[Tuple[str]] = None):
+    parser = argparse.ArgumentParser(prog="LINT")
+    parser.add_argument('path',
+                        help='path to directory you want to run pylint | '
+                        'Default: %(default)s | '
+                        'Type: %(type)s ',
+                        type=str)
+
+    parser.add_argument('-t',
+                        '--threshold',
+                        help='score threshold to fail pylint runner | '
+                        'Default: %(default)s | '
+                        'Type: %(type)s ',
+                        default=7,
+                        type=float)
+
+    args = parser.parse_args()
+    path = str(args.path)
+    threshold = float(args.threshold)
+
+    lint(path, threshold)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
