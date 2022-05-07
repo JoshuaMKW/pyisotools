JoshuaMKW/pyisotools

commit 13b6ec354576973689908f3de66856600f9f8115

Refactor according to pylint

pyisotools/bnrparser.py
@@ -27,7 +27,7 @@ class Jobs:
     EXTRACT = "EXTRACT"
 
 
-class RGB5A1(object):
+class RGB5A1():
 
     TileWidth = 4
     TileHeight = 4
@@ -35,25 +35,25 @@ class RGB5A1(object):
     @staticmethod
     def encode_pixel(pixel: tuple) -> int:
         """ RGB888 or RGBA8888 ONLY """
-        r = (pixel[0] >> 3) & 0b11111
-        g = (pixel[1] >> 3) & 0b11111
-        b = (pixel[2] >> 3) & 0b11111
-        a = 1
+        red = (pixel[0] >> 3) & 0b11111
+        green = (pixel[1] >> 3) & 0b11111
+        blue = (pixel[2] >> 3) & 0b11111
+        alpha = 1
 
-        return (a << 15) | (r << 10) | (g << 5) | b
+        return (alpha << 15) | (red << 10) | (green << 5) | blue
 
     @staticmethod
     def decode_pixel(pixel: int) -> tuple:
         """ RGBA8888 ONLY """
-        r = ((pixel >> 10) & 0b11111) << 3
-        g = ((pixel >> 5) & 0b11111) << 3
-        b = (pixel & 0b11111) << 3
-        a = 0xFF
+        red = ((pixel >> 10) & 0b11111) << 3
+        green = ((pixel >> 5) & 0b11111) << 3
+        blue = (pixel & 0b11111) << 3
+        alpha = 0xFF
 
-        return (r, g, b, a)
+        return (red, green, blue, alpha)
 
 
-class RGB5A3(object):
+class RGB5A3():
 
     TileWidth = 4
     TileHeight = 4
@@ -61,25 +61,25 @@ class RGB5A3(object):
     @staticmethod
     def encode_pixel(pixel: tuple) -> int:
         """ RGB888 or RGBA8888 ONLY """
-        r = (pixel[0] >> 4) & 0b1111
-        g = (pixel[1] >> 4) & 0b1111
-        b = (pixel[2] >> 4) & 0b1111
+        red = (pixel[0] >> 4) & 0b1111
+        green = (pixel[1] >> 4) & 0b1111
+        blue = (pixel[2] >> 4) & 0b1111
         if len(pixel) == 3:
-            a = 0b111
+            alpha = 0b111
         else:
-            a = (pixel[3] >> 5) & 0b111
+            alpha = (pixel[3] >> 5) & 0b111
 
-        return (a << 12) | (r << 8) | (g << 4) | b
+        return (alpha << 12) | (red << 8) | (green << 4) | blue
 
     @staticmethod
     def decode_pixel(pixel: int) -> tuple:
         """ RGBA8888 ONLY """
-        r = ((pixel >> 8) & 0b1111) << 4
-        g = ((pixel >> 4) & 0b1111) << 4
-        b = (pixel & 0b1111) << 4
-        a = ((pixel >> 12) & 0b111) << 5
+        red = ((pixel >> 8) & 0b1111) << 4
+        green = ((pixel >> 4) & 0b1111) << 4
+        blue = (pixel & 0b1111) << 4
+        alpha = ((pixel >> 12) & 0b111) << 5
 
-        return (r, g, b, a)
+        return (red, green, blue, alpha)
 
 
 class BNR(RGB5A3):
@@ -95,7 +95,17 @@ class BNR(RGB5A3):
     ImgTileWidth = ImageWidth // RGB5A1.TileWidth
     ImgTileHeight = ImageHeight // RGB5A1.TileHeight
 
-    def __init__(self, f: Path, region: Regions = Regions.AMERICA, gameName: str = "", gameTitle: str = "", developerName: str = "", developerTitle: str = "", desc: str = "", overwrite=False):
+    def __init__(
+        self,
+        f: Path,
+        region: Regions = Regions.AMERICA,
+        gameName: str = "",
+        gameTitle: str = "",
+        developerName: str = "",
+        developerTitle: str = "",
+        desc: str = "",
+        overwrite=False
+    ):
         self._rawdata = BytesIO(
             b"\x00" * (0x1960 if region != BNR.Regions.EUROPE else 0x1FA0))
         self.regionID = region
@@ -120,12 +130,11 @@ class BNR(RGB5A3):
     def region(self) -> str:
         if self.regionID == BNR.Regions.AMERICA:
             return "NTSC-U"
-        elif self.regionID == BNR.Regions.EUROPE:
+        if self.regionID == BNR.Regions.EUROPE:
             return "PAL"
-        elif self.regionID == BNR.Regions.JAPAN:
+        if self.regionID == BNR.Regions.JAPAN:
             return "NTSC-J"
-        else:
-            return "NTSC-K"
+        return "NTSC-K"
 
     @property
     @io_preserve
@@ -158,21 +167,23 @@ class BNR(RGB5A3):
         self._rawdata.seek(0x20)
         if isinstance(_img, bytes):
             self._rawdata.write(_img[:0x1800])
-        elif isinstance(_img, BytesIO):
+            return
+        if isinstance(_img, BytesIO):
             self._rawdata.write(_img.getvalue()[:0x1800])
-        elif isinstance(_img, Image.Image):
-            smallImg = _img.resize((BNR.ImageWidth, BNR.ImageHeight))
-            for yBlock in range(BNR.ImgTileHeight):
-                for xBlock in range(BNR.ImgTileWidth):
-                    for iy in range(BNR.TileHeight):
-                        for ix in range(BNR.TileWidth):
-                            x = xBlock*BNR.TileWidth + ix
-                            y = yBlock*BNR.TileHeight + iy
+            return
 
-                            pixel = smallImg.getpixel((x, y))
-                            if x < BNR.ImageWidth and y < BNR.ImageHeight:
-                                self._rawdata.write(self._encode_pixel(
-                                    pixel).to_bytes(2, "big", signed=False))
+        smallImg = _img.resize((BNR.ImageWidth, BNR.ImageHeight))
+        for blockRow in range(BNR.ImgTileHeight):
+            for blockColumn in range(BNR.ImgTileWidth):
+                for tileRow in range(BNR.TileHeight):
+                    for tileColumn in range(BNR.TileWidth):
+                        column = blockColumn*BNR.TileWidth + tileRow
+                        row = blockRow*BNR.TileHeight + tileColumn
+
+                        pixel = smallImg.getpixel((column, row))
+                        if column < BNR.ImageWidth and row < BNR.ImageHeight:
+                            self._rawdata.write(self._encode_pixel(
+                                pixel).to_bytes(2, "big", signed=False))
 
     @property
     @io_preserve
@@ -244,21 +255,22 @@ class BNR(RGB5A3):
         self._rawdata.write(
             bytes(name[:0x7F], "iso-8859-1" if self.region != "NTSC-J" else "shift-jis"))
 
-    def getImage(self) -> Image.Image:
+    def get_image(self) -> Image.Image:
         _image = self.rawImage
         img = Image.new("RGBA", (BNR.ImageWidth, BNR.ImageHeight))
 
-        for yBlock in range(BNR.ImgTileHeight):
-            for xBlock in range(BNR.ImgTileWidth):
-                for iy in range(BNR.TileHeight):
-                    for ix in range(BNR.TileWidth):
-                        x = xBlock*BNR.TileWidth + ix
-                        y = yBlock*BNR.TileHeight + iy
+        for blockRow in range(BNR.ImgTileHeight):
+            for blockColumn in range(BNR.ImgTileWidth):
+                for tileRow in range(BNR.TileHeight):
+                    for tileColumn in range(BNR.TileWidth):
+                        column = blockColumn*BNR.TileWidth + tileRow
+                        row = blockRow*BNR.TileHeight + tileColumn
 
                         pixel = int.from_bytes(
                             _image.read(2), "big", signed=False)
-                        if x < BNR.ImageWidth and y < BNR.ImageHeight:
-                            img.putpixel((x, y), self._decode_pixel(pixel))
+                        if column < BNR.ImageWidth and row < BNR.ImageHeight:
+                            img.putpixel(
+                                (column, row), self._decode_pixel(pixel))
 
         return img
 
@@ -266,7 +278,7 @@ class BNR(RGB5A3):
         dest.write_bytes(self._rawdata.getvalue())
 
     def save_png(self, dest: Path):
-        img = self.getImage()
+        img = self.get_image()
         img.save(str(dest))
 
         f = dest.parent / (dest.stem + ".json")
@@ -281,7 +293,17 @@ class BNR(RGB5A3):
         with f.open("w") as jsonFile:
             json.dump(info, jsonFile, indent=4)
 
-    def load(self, f: Path, region: Regions = Regions.AMERICA, gameName: str = "", gameTitle: str = "", developerName: str = "", developerTitle: str = "", desc: str = "", overwrite: bool = False):
+    def load(
+        self,
+        f: Path,
+        region: Regions = Regions.AMERICA,
+        gameName: str = "",
+        gameTitle: str = "",
+        developerName: str = "",
+        developerTitle: str = "",
+        desc: str = "",
+        overwrite=False
+    ):
         self.regionID = region
         if f.suffix == ".bnr":
             self._rawdata = BytesIO(f.read_bytes())
@@ -319,15 +341,13 @@ class BNR(RGB5A3):
     def _encode_pixel(pixel: tuple):
         if len(pixel) == 3:
             return RGB5A1.encode_pixel(pixel)
-        else:
-            return RGB5A3.encode_pixel(pixel)
+        return RGB5A3.encode_pixel(pixel)
 
     @staticmethod
     def _decode_pixel(pixel: int):
         if (pixel >> 15) & 1:
             return RGB5A1.decode_pixel(pixel)
-        else:
-            return RGB5A3.decode_pixel(pixel)
+        return RGB5A3.decode_pixel(pixel)
 
 
 if __name__ == "__main__":
@@ -371,18 +391,18 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.region == "E":
-        region = BNR.Regions.AMERICA
+        REGION = BNR.Regions.AMERICA
     elif args.region == "P":
-        region = BNR.Regions.EUROPE
+        REGION = BNR.Regions.EUROPE
     elif args.region == "J":
-        region = BNR.Regions.JAPAN
+        REGION = BNR.Regions.JAPAN
     else:
         raise NotImplementedError(
             f"Unknown region type {args.region} provided")
 
     if args.job == Jobs.COMPILE:
         bnr = BNR(Path(args.img).resolve(),
-                  region=region,
+                  region=REGION,
                   gameName=args.gamename,
                   gameTitle=args.gametitle,
                   developerName=args.devname,

.pylintrc
@@ -469,7 +469,8 @@ allow-global-unused-variables=yes
 # List of strings which can identify a callback function by name. A callback
 # name must start or end with one of those strings.
 callbacks=cb_,
-          _cb
+          _cb,
+          _callback
 
 # A regular expression matching the name of dummy variables (i.e. expected to
 # not be used).

pyisotools/__main__.py
@@ -1,6 +1,7 @@
 import sys
 
 from pathlib import Path
+from typing import Optional, Tuple
 
 from PySide2.QtCore import QCoreApplication
 from PySide2.QtWidgets import QApplication
@@ -10,7 +11,11 @@ from pyisotools.gui.mainwindow import Ui_MainWindow
 from pyisotools.gui.connector import Controller
 from pyisotools.iso import GamecubeISO
 
-def main(argv: list = sys.argv[1:]):
+
+def main(argv: Optional[Tuple] = None):
+    if argv is None:
+        argv = sys.argv[1:]
+
     if len(argv) == 0:
         app = QApplication()
         central = Ui_MainWindow()
@@ -18,13 +23,15 @@ def main(argv: list = sys.argv[1:]):
         central.setupUi(mainWindow)
         mainWindow.load_program_config()
         mainWindow.update_theme(mainWindow.theme)
-        mainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", f"pyisotools v{__version__}", None))
+        mainWindow.setWindowTitle(QCoreApplication.translate(
+            "MainWindow", f"pyisotools v{__version__}", None))
         mainWindow.show()
         sys.exit(app.exec_())
     else:
         from argparse import ArgumentParser
-        
-        parser = ArgumentParser(f"pyisotools v{__version__}", description="ISO tool for extracting/building Gamecube ISOs", allow_abbrev=False)
+
+        parser = ArgumentParser(
+            f"pyisotools v{__version__}", description="ISO tool for extracting/building Gamecube ISOs", allow_abbrev=False)
 
         parser.add_argument("src", help="ISO/root to build/extract with")
         parser.add_argument("job",
@@ -47,6 +54,7 @@ def main(argv: list = sys.argv[1:]):
             iso.build(args.dest)
         else:
             parser.print_help()
+
 
 if __name__ == "__main__":
     main()

pyisotools/apploader.py
@@ -5,7 +5,7 @@ from io import BytesIO
 from pyisotools.iohelper import read_string, read_uint32, write_uint32
 
 
-class Apploader(object):
+class Apploader():
 
     def __init__(self, f):
         f.seek(0x14, 1)

pyisotools/bi2.py
@@ -5,7 +5,7 @@ from io import BytesIO
 from pyisotools.iohelper import read_uint32, write_uint32
 
 
-class BI2(object):
+class BI2():
 
     class Country:
         JAPAN = 0

pyisotools/boot.py
@@ -6,7 +6,7 @@ from pyisotools.iohelper import (read_string, read_ubyte, read_uint32,
                                  write_ubyte, write_uint32)
 
 
-class Boot(object):
+class Boot():
 
     class Country:
         JAPAN = 0
@@ -67,7 +67,7 @@ class Boot(object):
     @property
     def audioStreaming(self) -> bool:
         self._rawdata.seek(8)
-        return True if self._rawdata.read(1) == b"\x01" else False
+        return bool(self._rawdata.read(1))
 
     @audioStreaming.setter
     def audioStreaming(self, active: bool):
@@ -89,10 +89,9 @@ class Boot(object):
         self._rawdata.seek(24)
         if read_uint32(self._rawdata) == Boot.Magic.WIIMAGIC:
             return Boot.Type.WII
-        elif read_uint32(self._rawdata) == Boot.Magic.GCNMAGIC:
+        if read_uint32(self._rawdata) == Boot.Magic.GCNMAGIC:
             return Boot.Type.GCN
-        else:
-            return Boot.Type.UNKNOWN
+        return Boot.Type.UNKNOWN
 
     @gameType.setter
     def gameType(self, _type: Boot.Type):

pyisotools/fst.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from fnmatch import fnmatch
 from pathlib import Path
-from typing import Union
+from typing import Optional, Union
 
 
 class FileAccessOnFolderError(Exception):
@@ -21,7 +21,7 @@ class InvalidFSTError(Exception):
     ...
 
 
-class FSTNode(object):
+class FSTNode():
 
     FILE = 0
     FOLDER = 1
@@ -184,8 +184,7 @@ class FSTNode(object):
     def size(self) -> int:
         if self.is_file():
             return self._filesize
-        else:
-            return self.num_children()
+        return self.num_children()
 
     @size.setter
     def size(self, size: int):
@@ -196,15 +195,15 @@ class FSTNode(object):
     def datasize(self) -> int:
         if self.is_file():
             return self._filesize
-        else:
-            size = sum((node.size for node in self.rfiles(includedOnly=True)))
-            return size
+
+        size = sum((node.size for node in self.rfiles(includedOnly=True)))
+        return size
 
     def find_by_path(self, path: Union[Path, str], skipExcluded: bool = True) -> FSTNode:
         _path = str(path).lower()
         doGlob = "?" in _path or "*" in _path
 
-        if _path == "" or _path == ".":
+        if _path in {"", "."}:
             return self.rootnode
 
         for node in self.rfiles(includedOnly=skipExcluded):
@@ -221,6 +220,8 @@ class FSTNode(object):
             else:
                 if node.path.lower() == _path:
                     return node
+
+        return None
 
     def add_child(self, node: FSTNode):
         self._children[node.name] = node
@@ -245,7 +246,7 @@ class FSTNode(object):
         return self.type == FSTNode.FILE
 
     def is_root(self) -> bool:
-        return self.type == FSTNode.FOLDER and self.name == "files" and self.parent == None
+        return self.type == FSTNode.FOLDER and self.name == "files" and self.parent is None
 
     def __eq__(self, other: FSTNode) -> bool:
         return self.name == other.name and self.type == other.type
@@ -256,8 +257,7 @@ class FSTNode(object):
     def __len__(self) -> int:
         if self.is_file():
             return self._filesize
-        else:
-            return self.num_children() + 1
+        return self.num_children() + 1
 
     def __bool__(self) -> bool:
         return True
@@ -268,11 +268,7 @@ class FSTNode(object):
                 if child == other:
                     return True
             return False
-        else:
-            if self.find_by_path(other):
-                return True
-            else:
-                return False
+        return bool(self.find_by_path(other))
 
 
 class FSTRoot(FSTNode):
@@ -287,7 +283,8 @@ class FSTRoot(FSTNode):
         for node in sorted(self.rfiles(), key=lambda x: x._fileoffset, reverse=reverse):
             yield node
 
-    def _detect_alignment(self, node: FSTNode, prev: FSTNode = None) -> int:
+    @staticmethod
+    def _detect_alignment(node: FSTNode, prev: Optional[FSTNode] = None) -> int:
         if prev:
             offset = node._fileoffset - (prev._fileoffset + prev.size)
         else:
@@ -310,11 +307,8 @@ class FSTRoot(FSTNode):
             if (offset & mask) == 0:
                 if mask + 1 <= alignment:
                     alignment = mask + 1
-                    found = True
-                    break
-                else:
-                    found = True
-                    break
+                found = True
+                break
             mask >>= 1
 
         if not found:
@@ -324,14 +318,11 @@ class FSTRoot(FSTNode):
 
 class FST(FSTRoot):
 
-    def __init__(self):
-        super().__init__()
-
     @property
     def strTableOfs(self) -> int:
         return len(self) * 0xC
 
-    def print_info(self, fst=None):
+    def print_info(self):
         def print_tree(node: FSTNode, string: str, depth: int) -> str:
             if node.is_file():
                 string += "  "*depth + node.name + "\n"

commit e25fd8e4746ccadfb13327e8963a2a3b12fbcc5f

Avoid mirroring tiles diagonally.
It seems this was a regression introduced by https://github.com/cristian64/pyisotools/commit/13b6ec354576973689908f3de66856600f9f8115,
where the tile column and the tile row were swapped inadvertently.

Because both the extract and the compile operations were suffering from
the same issue, two bads were making a good, and the resulting BNR file
after exporting/re-compiling was in fact good. However, the transitory
PNG image was corrupted.

Example of a corrupted PNG image:

![image](https://user-images.githubusercontent.com/1853278/167252802-f94822b3-47b2-4609-a4c3-d7f82aa71ff1.png)

Generated with:
```bash
cd pyisotools
python3 -m pyisotools.bnrparser .../GM4E01/files/opening.bnr /tmp/image.png -j EXTRACT
```

pyisotools/bnrparser.py
@@ -177,8 +177,8 @@ class BNR(RGB5A3):
             for blockColumn in range(BNR.ImgTileWidth):
                 for tileRow in range(BNR.TileHeight):
                     for tileColumn in range(BNR.TileWidth):
-                        column = blockColumn*BNR.TileWidth + tileRow
-                        row = blockRow*BNR.TileHeight + tileColumn
+                        column = blockColumn*BNR.TileWidth + tileColumn
+                        row = blockRow*BNR.TileHeight + tileRow
 
                         pixel = smallImg.getpixel((column, row))
                         if column < BNR.ImageWidth and row < BNR.ImageHeight:
@@ -263,8 +263,8 @@ class BNR(RGB5A3):
             for blockColumn in range(BNR.ImgTileWidth):
                 for tileRow in range(BNR.TileHeight):
                     for tileColumn in range(BNR.TileWidth):
-                        column = blockColumn*BNR.TileWidth + tileRow
-                        row = blockRow*BNR.TileHeight + tileColumn
+                        column = blockColumn*BNR.TileWidth + tileColumn
+                        row = blockRow*BNR.TileHeight + tileRow
 
                         pixel = int.from_bytes(
                             _image.read(2), "big", signed=False)